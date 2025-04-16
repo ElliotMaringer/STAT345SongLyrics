a5ed610028defedcc4aa0d632937848bbdd4bc82
ElliotMaringer <e9maringer@gmail.com>
2025-04-16 11:37:26 -0500

    Songs testing of the functions in utilities

--- Test.R
+++ Test.R
@@ -1,1 +1,10 @@
 ## This is a test to make sure the github is working.
+library(tidyverse)
+library(dplyr)
+top3songs <- getTop3Songs("Top100Songs.csv")
+cleanSongs <- clean_song_data(top3songs)
+
+songs <- read_csv("Top100Songs.csv")
+
+head(songs)
+=
